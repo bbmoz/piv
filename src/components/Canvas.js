833ec6608882f83d6a7dc6529070cf564b15049c
Joe Lee <blueberrymozart@gmail.com>
2017-04-16 20:06:52 -0500

    feat: canvas

---
+++ src/components/Canvas.js
@@ -0,0 +1,21 @@
+class Canvas {
+  constructor ($element, config) {
+    this.$element = $element
+    if (config) {
+      this.height = config.height
+      this.width = config.width
+      this._init()
+    }
+  }
+
+  _init () {
+    if (this.height !== undefined) this.$element.setAttribute('height', this.height)
+    if (this.width !== undefined) this.$element.setAttribute('width', this.width)
+  }
+
+  get context () {
+    return this.$element.getContext('2d')
+  }
+}
+
+export default Canvas
--- test/components/Canvas.js
+++ test/components/Canvas.js
@@ -1,3 +1,54 @@
 import test from 'ava'
+import $ from 'jsdom'
+import Canvas from './../../src/components/Canvas'
 
-test.todo('Canvas')
+test('new Canvas($element, { height, width })', t => {
+  t.plan(6)
+  const $element = doc.getElementById('canvas')
+  const height = 200
+  const width = 500
+
+  const canvas = new Canvas($element, {
+    height, width
+  })
+
+  // context
+  t.is(canvas.$element, $element)
+  t.is(canvas.height, height)
+  t.is(canvas.width, width)
+  t.is(canvas.context, $element.getContext('2d'))
+
+  // render
+  t.is($element.height, height)
+  t.is($element.width, width)
+})
+
+test('new Canvas(...): no config', t => {
+  t.plan(6)
+  const $element = doc.getElementById('canvas')
+
+  const canvas = new Canvas($element)
+
+  // context
+  t.is(canvas.$element, $element)
+  t.is(canvas.height, undefined)
+  t.is(canvas.width, undefined)
+  t.is(canvas.context, null)
+
+  // render
+  t.is($element.height, 150)
+  t.is($element.width, 300)
+})
+
+test.todo('new Canvas(...): missing config props')
+
+let doc
+test.beforeEach('setup', () => {
+  doc = $.jsdom(`
+    <html>
+      <body>
+        <canvas id="canvas"></canvas>
+      </body>
+    </html>
+  `)
+})
